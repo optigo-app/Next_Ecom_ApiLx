f65a57e63f4ae2f3987148e1d557891476fd1975
subrata <subrata@gmail.com>
2026-07-03 17:44:04 +0530

    bug in hoq

--- app/theme/malakanJwewls.web/Lookbook/page.js
+++
@@ -1,9 +0,0 @@
-import Lookbook from './Lookbook'
-
-const page = ({storeInit}) => {
-  return (
-    <Lookbook key={`lookbook-${storeInit?.token}`} storeInit={storeInit}/>
-  )
-}
-
-export default page
